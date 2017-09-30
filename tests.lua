f3f324e3c2f7c476941840e33739194818a775d0
George Zhao <zhaozg@gmail.com>
2017-09-30 15:33:18 +0800

    update tests.lua use luvit-tap

--- tests.lua
+++ tests.lua
@@ -1,6 +1,9 @@
-local cron = require 'cron'
+local cron = require './cron'
+local utils = require('utils')
+local dump = utils.dump
+local strip = utils.strip
 
-describe( 'cron', function()
+require('tap')(function (test)
 
   local counter
   local function count(amount)
@@ -9,131 +12,125 @@ describe( 'cron', function()
   end
   local countable = setmetatable({}, {__call = count})
 
-  before_each(function()
+  test('clock :update', function()
     counter = 0
+    local clock = cron.every(1, count)
+    assert(not pcall(function() clock:update() end))
+    assert(not pcall(function() clock:update(-1) end))
+    assert(pcall(function() clock:update(1) end))
   end)
 
-
-  describe('clock', function()
-
-    describe(':update', function()
-      it('throws an error if dt is not positive', function()
-        local clock = cron.every(1, count)
-        assert.error(function() clock:update() end)
-        assert.error(function() clock:update(-1) end)
-        assert.not_error(function() clock:update(1) end)
-      end)
-    end)
-
-    describe(':reset', function()
-      it('defaults to 0', function()
-        local clock = cron.every(1, count)
-        clock:update(1)
-        clock:reset(0)
-        assert.equal(clock.running, 0)
-      end)
-      it('throws an error if dt is not positive', function()
-        local clock = cron.every(1, count)
-        assert.error(function() clock:reset(-1) end)
-        assert.error(function() clock:reset('foo') end)
-        assert.not_error(function() clock:reset() end)
-        assert.not_error(function() clock:reset(1) end)
-      end)
-    end)
+  test('clock :reset defaults to 0', function()
+    counter = 0
+    local clock = cron.every(1, count)
+    clock:update(1)
+    clock:reset(0)
+    assert(clock.running == 0)
   end)
 
-
-  describe('.after', function()
-    it('checks parameters', function()
-      assert.error(function() cron.after('error', count) end)
-      assert.error(function() cron.after(2, 'error') end)
-      assert.error(function() cron.after(-2, count) end)
-      assert.error(function() cron.after(2, {}) end)
-      assert.not_error(function() cron.after(2, count) end)
-      assert.not_error(function() cron.after(2, countable) end)
-    end)
-
-    it('produces a clock that executes actions only once, at the right time', function()
-      local c1 = cron.after(2, count)
-      local c2 = cron.after(4, count)
-
-      -- 1
-      c1:update(1)
-      assert.equal(counter, 0)
-      c2:update(1)
-      assert.equal(counter, 0)
-
-      -- 2
-      c1:update(1)
-      assert.equal(counter, 1)
-      c2:update(1)
-      assert.equal(counter, 1)
-
-      -- 3
-      c1:update(1)
-      assert.equal(counter, 1)
-      c2:update(1)
-      assert.equal(counter, 1)
-
-      -- 4
-      c1:update(1)
-      assert.equal(counter, 1)
-      c2:update(1)
-      assert.equal(counter, 2)
-
-    end)
-
-    it('produces a clock that can be expired', function()
-      local c1 = cron.after(2, count)
-      assert.is_false(c1:update(1))
-      assert.is_true(c1:update(1))
-      assert.is_true(c1:update(1))
-    end)
-
-    it('Passes on parameters to the callback', function()
-      local c1 = cron.after(1, count, 2)
-      c1:update(1)
-      assert.equal(counter, 2)
-    end)
+  test('clock :reset throws an error if dt is not positive', function()
+    counter = 0
+    local clock = cron.every(1, count)
+    assert(not pcall(function() clock:reset(-1) end))
+    assert(not pcall(function() clock:reset('foo') end))
+    assert(pcall(function() clock:reset() end))
+    assert(pcall(function() clock:reset(1) end))
   end)
 
-  describe('.every', function()
-    it('checks parameters', function()
-      assert.error(function() cron.every('error', count) end)
-      assert.error(function() cron.every(2, 'error') end)
-      assert.error(function() cron.every(-2, count) end)
-      assert.error(function() cron.every(-2, {}) end)
-      assert.not_error(function() cron.every(2, count) end)
-      assert.not_error(function() cron.every(2, countable) end)
-    end)
+  test('clock .after checks parameters', function()
+    counter = 0
+    assert(not pcall(function() cron.after('error', count) end))
+    assert(not pcall(function() cron.after(2, 'error') end))
+    assert(not pcall(function() cron.after(-2, count) end))
+    assert(not pcall(function() cron.after(2, {}) end))
+    assert(pcall(function() cron.after(2, count) end))
+    assert(pcall(function() cron.after(2, countable) end))
+  end)
 
-    it('Invokes callback periodically', function()
-      local c = cron.every(3, count)
+  test('clock .after produces a clock that executes actions only once, at the right time', function()
+    counter = 0
+    local c1 = cron.after(2, count)
+    local c2 = cron.after(4, count)
 
-      c:update(1)
-      assert.equal(counter, 0)
+    -- 1
+    c1:update(1)
+    assert(counter == 0)
+    c2:update(1)
+    assert(counter == 0)
 
-      c:update(2)
-      assert.equal(counter, 1)
+    -- 2
+    c1:update(1)
+    assert(counter == 1)
+    c2:update(1)
+    assert(counter == 1)
 
-      c:update(2)
-      assert.equal(counter, 1)
+    -- 3
+    c1:update(1)
+    assert(counter == 1)
+    c2:update(1)
+    assert(counter == 1)
 
-      c:update(1)
-      assert.equal(counter, 2)
-    end)
+    -- 4
+    c1:update(1)
+    assert(counter == 1)
+    c2:update(1)
+    assert(counter == 2)
+  end)
 
-    it('Executes the same action multiple times on a single update if appropiate', function()
-      local c = cron.every(1, count)
-      c:update(2)
-      assert.equal(counter, 2)
-    end)
+  test('clock .after produces a clock that can be expired', function()
+    counter = 0
+    local c1 = cron.after(2, count)
+    assert(false == c1:update(1))
+    assert(true == c1:update(1))
+    assert(true == c1:update(1))
+  end)
 
-    it('Respects parameters', function()
-      local c = cron.every(1, count, 2)
-      c:update(2)
-      assert.equal(counter, 4)
-    end)
+  test('clock .after Passes on parameters to the callback', function()
+    counter = 0
+    local c1 = cron.after(1, count, 2)
+    c1:update(1)
+    assert(counter == 2)
+  end)
+
+  test('clock .every checks parameters', function()
+    counter = 0
+    assert(not pcall(function() cron.every('error', count) end))
+    assert(not pcall(function() cron.every(2, 'error') end))
+    assert(not pcall(function() cron.every(-2, count) end))
+    assert(not pcall(function() cron.every(-2, {}) end))
+    assert(pcall(function() cron.every(2, count) end))
+    assert(pcall(function() cron.every(2, countable) end))
+  end)
+
+  test('clock .every Invokes callback periodically', function()
+    counter = 0
+    local c = cron.every(3, count)
+
+    c:update(1)
+    assert(counter == 0)
+
+    c:update(2)
+    assert(counter == 1)
+
+    c:update(2)
+    assert(counter == 1)
+
+    c:update(1)
+    assert(counter == 2)
+  end)
+
+  test('clock .every Executes the same action multiple times on a single update if appropiate', function()
+    counter = 0
+    local c = cron.every(1, count)
+    c:update(2)
+    assert(counter == 2)
+  end)
+
+  test('clock .every Respects parameters', function()
+    counter = 0
+    local c = cron.every(1, count, 2)
+    c:update(2)
+    assert(counter == 4)
   end)
 
 end)
